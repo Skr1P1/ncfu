a0f4582869aff3b3c9f525b21ae683580078d620
Skr1P1 <skri_pi@mail.ru>
2021-02-26 11:00:20 +0300

    add Lr2

---
+++ LR2/numbers.py
@@ -0,0 +1,8 @@
+a = float(input('Введите число-->'))
+b = float(input('Введите число-->'))
+c = float(input('Введите число-->'))
+d = float(input('Введите число-->'))
+s1 = a + b
+s2 = c + d
+div = s1/s2
+print('Полученное число %.2f' % div)
---
+++ LR2/user.py
@@ -0,0 +1,4 @@
+n = input('What is you name?')
+o = input('How old are you?')
+l = input('Where are you live?')
+print('This is -', n, '\nIt is -', o, '\n(S)he live in -', l)
