84c276e365469e93fdf01318fb9a3fe24e1b18f4
Skr1P1 <skri_pi@mail.ru>
2021-03-15 12:53:28 +0300

    Update hard.py

--- LR3/hard.py
+++ LR3/hard.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import math
+import sys
 
-x = float(input('Введите числ х -->'))
-k = int(input('Введите числ k -->'))
-n = int(input('Введите числ n -->'))
-eps = 1e-10
+EPS = 1e-10
+if __name__ == '__main__':
+    x = int(input("Введите x "))
+    n = int(input('Введите n'))
+    if x == 0:
+        print("Неверное значение х", file=sys.stderr)
+        exit(1)
+    a = x
+    S, k = a, 1
+    while math.fabs(a) > EPS:
+        a *= (((x**2/4)**k)/math.factorial(k)*(math.factorial(k+n)))
+        S += a
+        k += 1
+        break
+# Вывести значение функции.
+    print(((x/2)**n)*a)
 
-while math.fabs(x) > eps:
-    a = (x*x/4)**k
-    b = math.factorial(k)*(math.factorial(k+n))
-    k+=1
-print(((x/2)**n)*a/b)
 
